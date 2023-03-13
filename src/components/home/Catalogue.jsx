b90d10df1605e14574f64bc4c7b86930ff7decc0
rubens <ruben.schrurs@student.hogent.be>
2023-03-13 12:24:02 +0100

    [new] CatalogueItem added

--- src/components/home/Catalogue.jsx
+++ src/components/home/Catalogue.jsx
@@ -1,10 +1,17 @@
 import '../../css/home/Catalogue.scss'
+import CatalogueItem from './CatalogueItem'
 
 export default function Catalogue() {
     return (
         <>
             <div className="catalogue">
                 <h2>Catalogue</h2>
+                <div className='catalogueReleases'>
+                    <CatalogueItem/>
+                    <CatalogueItem/>
+                    <CatalogueItem/>
+                    <CatalogueItem/>
+                </div>
             </div>
         </>
     )
---
+++ src/components/home/CatalogueItem.jsx
@@ -0,0 +1,22 @@
+import '../../css/home/CatalogueItem.scss'
+
+export default function CatalogueItem() {
+    return (
+        <>
+            <div className='releaseLarge'>
+                <div className='releaseCoverArt'></div>
+                <div className='releaseDescription'>
+                    <div className='releaseInfo'>
+                        <div className='releaseTitle'>
+                            <h5>ReleaseTitle</h5>
+                        </div>
+                        <div className='releaseNameAndPrice'>
+                            <h6 className='releaseName'>ArtistName</h6>
+                            <h5 className='releasePrice'>€ 15,00</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
